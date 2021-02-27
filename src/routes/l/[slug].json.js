48ca8a351528d5de8a2b24aec50e299016b0d699
jianzhi-1 <jianzhiw00@gmail.com>
2021-02-27 22:29:41 +0800

    added db query for links

--- src/routes/l/[slug].json.js
+++ src/routes/l/[slug].json.js
@@ -1,15 +1,45 @@
-export function get(req, res, next) {
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+async function getUrlDB(u) {
+
+	if (u.endsWith(".json")){
+		u = u.slice(3, u.length - 5);
+	} else {
+		u = u.slice(3, u.length);
+	}
+
+    console.log("READING FROM DB")
+    const urlExists = await prisma.link.findUnique({
+        where: {
+            url: u,
+        }
+    });
+    return urlExists
+}
+
+export async function get(req, res, next) {
 	// Check DB for link redirecting instructions and user traffic
 
 	const { slug } = req.params;
 
-	if (false) {
-		res.writeHead(200, {
-			'Content-Type': 'application/json'
-		});
+	console.log(req.url)
 
-		res.end(lookup.get(slug));
-	} else {
+    const msg = await getUrlDB(req.url).then((result) => {
+        return result;
+    }).catch(e => {
+        throw e
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
+
+	console.log("MESSAGE")
+	console.log(msg)
+
+	if (msg == null || (!("url" in msg))){
+
 		res.writeHead(404, {
 			'Content-Type': 'application/json'
 		});
@@ -17,5 +47,18 @@ export function get(req, res, next) {
 		res.end(JSON.stringify({
 			message: `Link not found`
 		}));
+	} else {
+
+		console.log("LINK FOUND")
+		console.log(msg.map_url)
+		res.writeHead(201, {
+			'Content-Type': 'application/json'
+		});
+
+		res.end(JSON.stringify({
+			message: msg.map_url
+		}));
+
+		//res.end(lookup.get(slug));
 	}
 }

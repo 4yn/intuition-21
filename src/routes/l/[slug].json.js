9aee93a9eddb263dc2fa76591232ec0695d94f37
jianzhi-1 <jianzhiw00@gmail.com>
2021-02-27 23:05:50 +0800

    added user tracking feature

--- src/routes/l/[slug].json.js
+++ src/routes/l/[slug].json.js
@@ -2,13 +2,17 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
+function getUrl(u){
+	if (u.endsWith(".json")){
+		return u.slice(3, u.length - 5);
+	} else {
+		return u.slice(3, u.length);
+	}
+}
+
 async function getUrlDB(u) {
 
-	if (u.endsWith(".json")){
-		u = u.slice(3, u.length - 5);
-	} else {
-		u = u.slice(3, u.length);
-	}
+	u = getUrl(u);
 
     console.log("READING FROM DB")
     const urlExists = await prisma.link.findUnique({
@@ -19,12 +23,56 @@ async function getUrlDB(u) {
     return urlExists
 }
 
+async function numTimeVisited(sess, u) {
+
+	u = getUrl(u);
+
+    const nt = await prisma.user.findMany({
+        where: {
+            session: sess,
+			link_visited: u
+        }
+    });
+	console.log("IN NUM VISITED")
+	console.log(nt)
+
+    return nt
+}
+
+async function createUser(sess, u) {
+
+	u = getUrl(u);
+
+    const mm = await prisma.user.create({
+        data: {
+            session: sess,
+			link_visited: u
+        }
+    });
+
+	console.log("ENTRY CREATED WITH MSG")
+	console.log(mm)
+
+    return mm
+}
+
 export async function get(req, res, next) {
 	// Check DB for link redirecting instructions and user traffic
 
 	const { slug } = req.params;
 
 	console.log(req.url)
+	console.log(req.session)
+	console.log(req.sessionID)
+
+	const tt = await numTimeVisited(req.sessionID, req.url)
+	.then((result) => {return result;})
+	.catch(e => {throw e})
+	.finally(async()=>{
+		await prisma.$disconnect()
+	})
+
+	console.log("USER LOGGED IN " + (tt.length).toString() + " TIMES")
 
     const msg = await getUrlDB(req.url).then((result) => {
         return result;
@@ -51,12 +99,20 @@ export async function get(req, res, next) {
 
 		console.log("LINK FOUND")
 		console.log(msg.map_url)
+
+		const ww = await createUser(req.sessionID, req.url)
+		.then((result) => {return result;})
+		.catch(e => {throw e})
+		.finally(async()=>{
+			await prisma.$disconnect()
+		})
+
 		res.writeHead(201, {
 			'Content-Type': 'application/json'
 		});
 
 		res.end(JSON.stringify({
-			message: msg.map_url
+			message: "Visited " + msg.map_url + " A TOTAL OF " + (tt.length).toString()
 		}));
 
 		//res.end(lookup.get(slug));

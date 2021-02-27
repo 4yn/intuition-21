279669e8607134f2614afdaed2711f53ad0aa1de
jianzhi-1 <jianzhiw00@gmail.com>
2021-02-28 04:09:45 +0800

    minor default change

--- src/routes/store.js
+++ src/routes/store.js
@@ -2,15 +2,15 @@
 import { writable } from 'svelte/store';
 
 export const link = writable({
-    url: "heehee",
-    map_url: "www.google6.com",
-    title: "PRANK YOU",
-    body: "https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png",
-    thumbnail_link: "www.cat.com",
+    url: "suslink",
+    map_url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    title: "SEEMS SUS",
+    body: "you are in for a surprise...",
+    thumbnail_link: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/03-the-joker-w1200-h630-1562679871.jpg",
     routing: "CYCLE",
-    tl1: "www.google1.com",
-    tl2: "www.google2.com",
-    tl3: "www.google3.com",
-    tl4: "www.google4.com",
-    tl5: "www.google5.com",
+    tl1: "https://www.youtube.com/watch?v=s3Q80mk7bxE",
+    tl2: "https://www.youtube.com/watch?v=DVx8L7a3MuE",
+    tl3: "https://www.youtube.com/watch?v=bc0KhhjJP98",
+    tl4: "https://www.youtube.com/watch?v=mrbEKl0M3wQ",
+    tl5: "https://www.youtube.com/watch?v=JCQVnSOFqfM",
 })
